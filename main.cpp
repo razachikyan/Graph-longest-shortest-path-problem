5740740dcc78213493c3a711a6ab4dad4c95ed4a
unknown <razmik.achikyan@solicy.net>
2024-01-18 11:19:07 +0400

    fixes

--- main.cpp
+++ main.cpp
@@ -3,10 +3,34 @@
 int main() {
     Graph graph;
     graph.inputGraph();
+
+    std::cout << "Your matrix >>\n";
+
     graph.printMatrix();
-    std::vector<int> path = graph.astar();
-    for(const auto& node: path) {
-        std::cout << node << ", ";
+
+    std::cout << "Print the longest path(L).\nPrint the shortest path between 2 nodes(S)\n";
+    std::string ans;
+    std::cin >> ans;
+    std::cout << std::endl;
+
+    if (ans == "L") {
+        std::vector<int> longPath = graph.getLongestPath();
+
+        for (const auto& node : longPath) {
+            std::cout << node << ", ";
+        }
     }
+    else if (ans == "S") {
+        std::vector<int> path = graph.astar();
+
+        for (const auto& node : path) {
+            std::cout << node << ", ";
+        }
+    }
+    else {
+        std::cout << "Wrong answer\n";
+    }
+
+
     return 0;
 }
